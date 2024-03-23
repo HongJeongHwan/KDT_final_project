446ce62a021c96c3fe2597db509970f38746bf1d
HongJeongHwan <71goodhabits@gmail.com>
2024-03-23 11:10:10 +0900

    postgresql add

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,3 +4,14 @@ ALLOWED_HOSTS = ['52.79.213.179']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pybo',
+        'USER': 'dbmasteruser',
+        'PASSWORD': 'votmdnjem1234',
+        'HOST': 'ls-899b0b24494e190f9d6363bf5503b0b30395fb2a.c3ms6ca44pmh.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
